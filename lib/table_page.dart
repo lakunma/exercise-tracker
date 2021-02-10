c5272de98eaa552eb30115b6909bf617ea39d868
lakunma <lakunma@users.noreply.github.com>
2021-02-10 10:02:03 +0300

    table investigations

---
+++ lib/table_page.dart
@@ -0,0 +1,47 @@
+import 'package:editable/editable.dart';
+import 'package:flutter/material.dart';
+
+//try also https://pub.dev/packages/pluto_grid
+
+class TablePage extends StatefulWidget {
+  @override
+  _TablePageState createState() => _TablePageState();
+}
+class _TablePageState extends State<TablePage> {
+  //row data
+  List rows = [
+      {"name": 'James Peter', "date":'01/08/2007',"month":'March',"status":'beginner'},
+      {"name": 'Okon Etim', "date":'09/07/1889',"month":'January',"status":'completed'},
+      {"name": 'Samuel Peter', "date":'11/11/2002',"month":'April',"status":'intermediate'},
+      {"name": 'Udoh Ekong', "date":'06/3/2020',"month":'July',"status":'beginner'},
+      {"name": 'Essien Ikpa', "date":'12/6/1996',"month":'June',"status":'completed'},
+  ];
+//Headers or Columns
+  List headers = [
+      {"title":'Name', 'index': 1, 'key':'name'},
+      {"title":'Date', 'index': 2, 'key':'date'},
+      {"title":'Month', 'index': 3, 'key':'month'},
+      {"title":'Status', 'index': 4, 'key':'status'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Editable(
+          columns: headers,
+          rows: rows,
+          showCreateButton: true,
+          tdStyle: TextStyle(fontSize: 20),
+          showSaveIcon: true,
+          borderColor: Colors.grey.shade300,
+          onSubmitted: (value){
+            print(value);
+          },
+          onRowSaved: (value){
+            print("saved $value");
+          }
+
+        )
+    );
+  }
+}
